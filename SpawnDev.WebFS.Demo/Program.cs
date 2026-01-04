edcda48b744e8ef9ac03e7c79461c7026893ed24
Todd Tanner <todd@spawndev.com>
2026-01-04 13:08:04 -0500

    Dependency update. Added option to run in  SharedWorker to Program.cs

--- SpawnDev.WebFS.Demo/Program.cs
+++ SpawnDev.WebFS.Demo/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SpawnDev;
 using SpawnDev.BlazorJS;
 using SpawnDev.BlazorJS.WebWorkers;
 using SpawnDev.WebFS;
@@ -7,7 +8,7 @@ using SpawnDev.WebFS.Demo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// Add SpawnDev.BlazorJS JS  interop
+// Add SpawnDev.BlazorJS JS interop
 builder.Services.AddBlazorJSRuntime(out var JS);
 // WebWorkerService allows running the file system provider in a worker if we want
 builder.Services.AddWebWorkerService();
@@ -16,31 +17,48 @@ builder.Services.AddWebWorkerService();
 // Registers WebFSClient which connects to the tray app when its Enabled property is set to true
 builder.Services.AddWebFS<WebFSProvider>();
 
-// if set to true, the WebFSProvider service will be enabled in a web worker instead of the window
-var runProviderInWorker = false;
-
+// If this is a window, add window componenets
 if (JS.IsWindow)
 {
     builder.RootComponents.Add<App>("#app");
     builder.RootComponents.Add<HeadOutlet>("head::after");
 }
-// Startup using BlazorJSRunAsync
+
+// Start background services that need it
 var host = await builder.Build().StartBackgroundServices();
+
+// If this is a window, start the file system provider based on scope we want to run it in
 if (JS.IsWindow)
 {
-    if (runProviderInWorker)
+    // providerScope options:
+    // Window (default if not one of others) - runs in the window scope. Best for testing.
+    // DedicatedWorker - runs in a dedicated worker. Breakpoints and debugging not supported in workers.
+    // SharedWorker - runs in a shared worker. Breakpoints and debugging not supported in workers.
+    var providerScope = GlobalScope.Window;
+    if (providerScope == GlobalScope.DedicatedWorker)
     {
-        // running in a window with runProviderInWorker == true. start a worker to host the fs
+        // run in a DedicatedWorker
         // NOTE: Currently the ui will not display activity if the provider runs in a worker
         var WebWorkerService = host.Services.GetRequiredService<WebWorkerService>();
         var webWorker = await WebWorkerService.GetWebWorker();
+        // start the provider in the dedicatd worker
+        await webWorker!.Set<WebFSProvider, bool>(WebFSProvider => WebFSProvider.Enabled, true);
+    }
+    else if (providerScope == GlobalScope.SharedWorker)
+    {
+        // run in a SharedWorker
+        // NOTE: Currently the ui will not display activity if the provider runs in a worker
+        var WebWorkerService = host.Services.GetRequiredService<WebWorkerService>();
+        var webWorker = await WebWorkerService.GetSharedWebWorker("FileSystemProvider");
+        // start the provider in the shared worker
         await webWorker!.Set<WebFSProvider, bool>(WebFSProvider => WebFSProvider.Enabled, true);
     }
     else
-    {
-        // running in a window with runProviderInWorker == false. start the fs provider here in the window
+    { 
+        // run in the Window scope
         var WebFSProvider = host.Services.GetRequiredService<WebFSProvider>();
         WebFSProvider.Enabled = true;
     }
 }
+// Startup using BlazorJSRunAsync
 await host.BlazorJSRunAsync();
